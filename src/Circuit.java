fabfc61f18dd764d99d53af481f6666a52dfb7cd
Xyzer <xyzer.mail@gmail.com>
2015-06-10 22:16:24 +0200

    Basic node making

--- src/Circuit.java
+++ src/Circuit.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import nodes.Node;
+
 public class Circuit {
 	
 	private NodeFactory nodeFactory = new NodeFactory();
@@ -15,18 +17,24 @@ public class Circuit {
 	public Circuit(String fileName, int carryIn){
 		this.carryIn = carryIn;
 		readFile(fileName);
-		createCircuit();
 	}
 	
 	
 	
 	
 	
-	private void createCircuit() {;
+	private void setNodeObserver() {
 		
+		ArrayList<Node> nodeList = this.nodeFactory.getNodeList();
+		System.out.println(nodeList.size());
+		
+	}
+	
+	private void createNode (String type) {
+		type = type.substring(0, type.indexOf(';'));
+		type = type.substring(type.indexOf(":")+1, type.length());
 		// nodeType (String) can be: AND, NAND, NOR, NOT, OR, XOR
-		this.nodeFactory.createNode("AND");
-		
+		this.nodeFactory.createNode(type);
 	}
 
 
@@ -34,31 +42,45 @@ public class Circuit {
 
 
 	public boolean readFile(String fileName){
-			lines = new ArrayList<String>();
-	        try
-	        {
-	        	FileReader fr = new java.io.FileReader(fileName);
-	            BufferedReader br = new BufferedReader(fr);
-	            String s;
-	            while ((s = br.readLine()) != null)
-	            {
-	                s = s.replaceAll("\\t", "").replaceAll("\\n", "").replaceAll(" ", "");
-	                this.lines.add(s);
-	            }
+		lines = new ArrayList<String>();
+		try
+        {
+			FileReader fr = new java.io.FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+            String s;
+            boolean nodeCreation = true;
+            while ((s = br.readLine()) != null)
+            {
+            	s = s.replaceAll("\\t", "").replaceAll("\\n", "").replaceAll(" ", "");
+                if (!s.contains("#") && s.contains("NODE") && nodeCreation) {
+	                createNode(s);
+                } else if(!s.contains("#") && s.contains("A:") && nodeCreation) {
+                	a = s.contains("INPUT_HIGH") ? 1 : 0;
+                	System.out.println("A in is: "+a);
+                } else if(!s.contains("#") && s.contains("B:") && nodeCreation) {
+                	b = s.contains("INPUT_HIGH") ? 1 : 0;
+                	System.out.println("B in is: "+b);
+                } else if(!s.contains("#") && s.contains("Cin:") && nodeCreation) {
+                	carryIn = s.contains("INPUT_HIGH") ? 1 : 0;
+                	System.out.println("Carry in is: "+carryIn);
+            	// Find the blank line to switch to node linking
+                } else if("".equals(s.trim())) {
+                	nodeCreation = false;
+                // Not nodeCreation so it's time to link the nodes
+                } else if(!s.contains("#") && s.contains("NODE") && !nodeCreation) {
+                	System.out.println("NODE LINK:" + s);
+                }
+            }
 
-	            fr.close();
-	        }
-	        catch (FileNotFoundException e){ 
-	        	System.out.println("File: " + fileName + " does not exist. Exiting program."); System.exit(1); 
-	        	}
-	        catch (IOException e){ 
-	        	System.out.println("Failed to read file: " + fileName + ". Exiting program."); System.exit(1); 
-	        	}
-		return true;
+            fr.close();
+        }
+        catch (FileNotFoundException e){ 
+        	System.out.println("File: " + fileName + " does not exist. Exiting program."); System.exit(1); 
+    	}
+        catch (IOException e){ 
+        	System.out.println("Failed to read file: " + fileName + ". Exiting program."); System.exit(1); 
+    	}
+	    return true;
 	}
-	
-	
-	
-	
 
 }
