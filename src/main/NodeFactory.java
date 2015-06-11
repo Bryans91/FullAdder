544787737a4d4e492c088e61df6608086cdf96e4
Xyzer <xyzer.mail@gmail.com>
2015-06-11 03:15:26 +0200

    Debuggingg

--- src/main/NodeFactory.java
+++ src/main/NodeFactory.java
@@ -15,16 +15,22 @@ public class NodeFactory {
 		// Make a node depending on the nodeType
 		if (nodeType.equals("AND")) {
 			node = new NodeAnd();
+			node.setInputCount(2);
 		} else if (nodeType.equals("NAND")) {
 			node = new NodeNand();
+			node.setInputCount(2);
 		} else if (nodeType.equals("NOR")) {
 			node = new NodeNor();
+			node.setInputCount(2);
 		} else if (nodeType.equals("NOT")) {
 			node = new NodeNot();
+			node.setInputCount(1);
 		} else if (nodeType.equals("OR")) {
 			node = new NodeOr();
+			node.setInputCount(2);
 		} else if (nodeType.equals("XOR")) {
 			node = new NodeXor();
+			node.setInputCount(2);
 		} else if (nodeType.equals("PROBE")) {
 			Probe probe = new Probe(); // TODO
 		}
