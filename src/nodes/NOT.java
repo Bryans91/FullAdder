042929c93793e1a3af69f333934b6066e3848ff9
Xyzer <Xyzer@users.noreply.github.com>
2018-09-07 19:12:22 +0200

    It had to be done, ik ga niks anders openen..

--- src/nodes/NOT.java
+++ src/nodes/NOT.java
@@ -6,13 +6,7 @@ public class NOT extends Node {
 	
 	public int doCalc(){
 		System.out.println("NOT:"+this.input.get(0));
-		int out = 0;
-		
-		if(this.input.get(0) == 0){
-			out = 1;
-		}
-		
-		return out;
+		return 1 - this.input.get(0);
 	}
 
 }
